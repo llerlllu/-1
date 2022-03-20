26006ec545c65444099003a95a1b89451d86e245
llerlllu <llerlllu@gmail.com>
2022-03-20 17:59:24 +0300

    session_1

---
+++ llerlllu/Page2.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace llerlllu
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int in1, in2, result;
+            in1 = int.Parse(Text1.Text);
+            in2 = int.Parse(Text2.Text);
+            result = in1 - in2;
+            Text3.Text = result.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Text1.Text = null;
+            Text2.Text = null;
+            Text3.Text = null;
+        }
+    }
+}
---
+++ llerlllu/Page3.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace llerlllu
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int in1, in2, result;
+            in1 = int.Parse(Text1.Text);
+            in2 = int.Parse(Text2.Text);
+            result =Convert.ToInt32(Math.Pow(in1, in2));
+            Text3.Text = result.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Text1.Text = null;
+            Text2.Text = null;
+            Text3.Text = null;
+        }
+    }
+}
